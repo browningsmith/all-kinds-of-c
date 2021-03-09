1924394e60c9853f9f194783d0ea57ca30f082ef
Keith Smith <browningsmith@email.arizona.edu>
2021-03-08 17:29:03 -0700

    created RBTNode struct

--- UNIX_and_Linux/data-structures/rbt/rbt.h
+++ UNIX_and_Linux/data-structures/rbt/rbt.h
@@ -1,4 +1,16 @@
 #ifndef RBT_H
 #define RBT_H
 
+typedef struct RBTNode_struct {
+
+    short isRed; // zero if black, non-zero if red
+
+    struct RBTNode_struct* parent;
+    struct RBTNode_struct* left;
+    struct RBTNode_struct* right;
+
+    void* content;
+
+} RBTNode;
+
 #endif /* RBT_H */
