abc401be38e13c125866074a94e1b38dd730e556
Keith Smith <browningsmith@email.arizona.edu>
2021-03-18 18:06:18 -0700

    Messed around with void pointers and casting, testing a function that compares two integers

--- UNIX_and_Linux/data-structures/rbt/rbtest.c
+++ UNIX_and_Linux/data-structures/rbt/rbtest.c
@@ -3,41 +3,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int compareInt(void* a, void* b);
+
+int main(int argc, char** argv)
 {
+    if (argc < 3)
+    {
+        printf("ERROR: Need to enter two numbers to compare\n");
+        printf("Usage: ./rbtest <numberA> <numberB>\n");
+        return -1;
+    }
+    
     printf("rbtest startup\n");
 
     // Test creating a new RBT
     RBT tree;
     rbtInit(&tree);
     
-    // Test rbtIsTreeEmpty to be true before any nodes are inserted
-    printf("Testing that new init tree is empty: ");
-    if (!rbtIsTreeEmpty(tree)) // rbtIsTreeEmpty returns 1 if tree is empty
+    // Test creating two nodes that contain integers
+    printf("Creating nodeA: ");
+    RBTNode* nodeA = rbtNewNode(malloc(sizeof(int)));
+    if (nodeA == NULL)
     {
-        printf("Tree is not empty!\n");
+        perror("Unable to create nodeA");
         return -1;
     }
-    printf("Tree is empty!\n");
+    else if (rbtIsNodeEmpty(*nodeA))
+    {
+        perror("Created nodeA, but was unable to create content");
+        return -1;
+    }
+    printf("Created nodeA\n");
+    *((int*) nodeA->content) = atoi(argv[1]);
+    printf("Filled nodeA with %i\n", *((int*) nodeA->content));
 
-    // Manually add a new node to the tree using rbtNewNode
-    printf("Testing manually adding new empty node to tree: ");
-    tree.head = rbtNewNode(NULL);
-    if (rbtIsTreeEmpty(tree)) // rbtNewNode returns NULL on error, so if rbtIsTreeEmpty is 1, there was an error
+    printf("Creating nodeB: ");
+    RBTNode* nodeB = rbtNewNode(malloc(sizeof(int)));
+    if (nodeB == NULL)
     {
-        perror("Error creating new empty node for tree");
+        perror("Unable to create nodeB");
         return -1;
     }
-    printf("Manually created new empty node for tree\n");
+    else if (rbtIsNodeEmpty(*nodeB))
+    {
+        perror("Created nodeB, but was unable to create content");
+        return -1;
+    }
+    printf("Created nodeB\n");
+    *((int*) nodeB->content) = atoi(argv[2]);
+    printf("Filled nodeB with %i\n", *((int*) nodeB->content));
 
-    // Test that this new node is empty
-    printf("Testing that this new node is empty: ");
-    if (!rbtIsNodeEmpty(*tree.head)) // rbtIsNodeEmpty returns 1 if the node's content is empty
-    {
-        printf("Error, node is apparently not empty\n");
-        return -1;
-    }
-    printf("Node is empty\n");
+    // Test using custom compare function to compare the two integers
+    compareInt(nodeA->content, nodeB->content);
 
     return 0;
+}
+
+int compareInt(void* a, void* b)
+{
+    int derefA = *((int*) a);
+    int derefB = *((int*) b);
+
+    printf("Comparing integers A: %i and B: %i\n", derefA, derefB);
+
+    if (derefA == derefB)
+    {
+        printf("A is equal to B\n");
+        return 0;
+    }
+    else if (derefA < derefB)
+    {
+        printf("A is less than B\n");
+        return -1;
+    }
+    else
+    {
+        printf("A is greater than B\n");
+        return 1;
+    }
 }
