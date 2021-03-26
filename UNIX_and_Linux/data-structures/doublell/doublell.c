1df592be6ee9ee8930af595c44d12ef09abfcec0
Keith Smith <browningsmith@email.arizona.edu>
2021-03-25 19:23:17 -0700

    created dllInit to initialize the values of an empty dll

--- UNIX_and_Linux/data-structures/doublell/doublell.c
+++ UNIX_and_Linux/data-structures/doublell/doublell.c
@@ -1,1 +1,9 @@
 #include "doublell.h"
+
+#include <stdlib.h>
+
+void dllInit(DLL* list)
+{
+    list->start = NULL;
+    list->end = NULL;
+}
--- UNIX_and_Linux/data-structures/doublell/doublell.h
+++ UNIX_and_Linux/data-structures/doublell/doublell.h
@@ -1,4 +1,35 @@
 #ifndef DOUBLELL_H
 #define DOUBLELL_H
 
+typedef struct DLLNode_struct {
+
+    struct DLLNode_struct* prev;
+    struct DLLNode_struct* next;
+
+    void* content;
+
+} DLLNode;
+
+typedef struct {
+
+    DLLNode* start;
+    DLLNode* end;
+
+} DLL;
+
+/*****************************************************************
+ * dllInit
+ * 
+ * Inputs: DLL* list
+ * Returns: None
+ * 
+ * Initializes the given list as a new empty doubly linked list
+ *
+ * If a pointer to an already existing DLL is passed, all data
+ * within the list will be lost to a memory leak, as this function
+ * does not clear out an already existing list
+ *****************************************************************/
+
+void dllInit(DLL* list);
+
 #endif /* DOUBLELL_H */
