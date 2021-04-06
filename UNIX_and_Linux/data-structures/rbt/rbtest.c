ab40ab03916feeca64990f71a6a0b9bc7bce275c
Keith Smith <browningsmith@email.arizona.edu>
2021-04-06 16:32:54 -0700

    Created newIntNode, displayNode, and constructTree to rbtest.c to help with testing

--- UNIX_and_Linux/data-structures/rbt/rbtest.c
+++ UNIX_and_Linux/data-structures/rbt/rbtest.c
@@ -3,17 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include "rbtimpl.h"
 
 int compareInt(void* a, void* b);
+RBTNode* newIntNode(int num);
+void displayNode(RBTNode node);
+void constructTree(RBT* tree);
 
 int main()
 {
     RBT tree;
     
     printf("Running tests on rbt\n");
-
+    printf("Sizeof int: %lu\n", sizeof(int));
+    printf("Sizeof RBTNode: %lu\n", sizeof(RBTNode));
+    printf("Sizeof RBTNode with int: %lu\n", sizeof(RBTNode) + sizeof(int));
+    
     // Test rbtStatusAsText
     printf("Testing rbtStatusAsText\n");
     {
@@ -93,6 +100,15 @@ int main()
     }
     printf("Completed rbtIsEmpty\n");
 
+    // Test rbtGetNodeFromStart__
+    printf("Testing rbtGetNodeFromStart__\n");
+    {
+        rbtInit(&tree, compareInt);
+
+        constructTree(&tree);
+    }
+    printf("Completed rbtGetNodeFromStart__\n");
+
     printf("Tests complete\n");
 
     return 0;
@@ -122,3 +138,149 @@ int compareInt(void* a, void* b)
     }
 }
 
+RBTNode* newIntNode(int num)
+{
+    void* content = malloc(sizeof(int));
+    if (content == NULL)
+    {
+        printf("Unable to allocate space for int %i: %s\n", num, strerror(errno));
+        exit(-1);
+    }
+    *(int*) content = num;
+
+    RBTNode* newNode = malloc(sizeof(RBTNode));
+    if (newNode == NULL)
+    {
+        printf("Unable to allocate space for node %i: %s\n", num, strerror(errno));
+        exit(-1);
+    }
+
+    newNode->content = content;
+    newNode->isRed = 1;
+    newNode->parent = NULL;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+void displayNode(RBTNode node)
+{
+    printf("Color: ");
+    if (node.isRed)
+    {
+        printf("RED\n");
+    }
+    else
+    {
+        printf("BLACK\n");
+    }
+
+    printf("Content: ");
+    if (node.content == NULL)
+    {
+        printf("NULL\n");
+    }
+    else
+    {
+        printf("%i\n", *(int*) node.content);
+    }
+
+    printf("Parent: ");
+    if (node.parent == NULL)
+    {
+        printf("NULL\n");
+    }
+    else if (node.parent->content == NULL)
+    {
+        printf("NULL_CONTENT\n");
+    }
+    else
+    {
+        printf("%i\n", *(int*) node.parent->content);
+    }
+
+    printf("Left: ");
+    if (node.left == NULL)
+    {
+        printf("NULL\n");
+    }
+    else if (node.left->content == NULL)
+    {
+        printf("NULL_CONTENT\n");
+    }
+    else
+    {
+        printf("%i\n", *(int*) node.left->content);
+    }
+
+    printf("Right: ");
+    if (node.right == NULL)
+    {
+        printf("NULL\n");
+    }
+    else if (node.right->content == NULL)
+    {
+        printf("NULL_CONTENT\n");
+    }
+    else
+    {
+        printf("%i\n", *(int*) node.right->content);
+    }
+}
+
+void constructTree(RBT* tree)
+{
+    rbtInit(tree, compareInt);
+
+    // 400 Node (black)
+    RBTNode* node400 = newIntNode(400);
+    node400->isRed = 0;
+
+    // 200 Node (black)
+    RBTNode* node200 = newIntNode(200);
+    node200->isRed = 0;
+    node400->left = node200;
+    node200->parent = node400;
+
+    // 600 Node (black)
+    RBTNode* node600 = newIntNode(600);
+    node600->isRed = 0;
+    node400->right = node600;
+    node600->parent = node400;
+
+    // 100 Node (red)
+    RBTNode* node100 = newIntNode(100);
+    node200->left = node100;
+    node100->parent = node200;
+
+    // 300 Node (red)
+    RBTNode* node300 = newIntNode(300);
+    node200->right = node300;
+    node300->parent = node200;
+
+    // 500 Node (red)
+    RBTNode* node500 = newIntNode(500);
+    node600->left = node500;
+    node500->parent = node600;
+
+    // 700 Node (red)
+    RBTNode* node700 = newIntNode(700);
+    node600->right = node700;
+    node700->parent = node600;
+
+    // printf("Node 400:\n");
+    // displayNode(*node400);
+    // printf("\nNode 200:\n");
+    // displayNode(*node200);
+    // printf("\nNode 600:\n");
+    // displayNode(*node600);
+    // printf("\nNode 100:\n");
+    // displayNode(*node100);
+    // printf("\nNode 300:\n");
+    // displayNode(*node300);
+    // printf("\nNode 500:\n");
+    // displayNode(*node500);
+    // printf("\nNode 700:\n");
+    // displayNode(*node700);
+}
