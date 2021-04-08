280c347c6c68ec014d118894de3b6845567def90
Keith Smith <browningsmith@email.arizona.edu>
2021-04-08 07:15:22 -0700

    Added some code to itoa.c, but it is not yet finished

--- UNIX_and_Linux/cooltools/itoa.c
+++ UNIX_and_Linux/cooltools/itoa.c
@@ -4,9 +4,43 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int itoa(char* buf, int32_t num)
+char* itoa(char* buf, int32_t num)
 {
-    printf("Turning %i into words since 2021!\n", (int) num);
+    if (buf == NULL)
+    {
+        return buf;
+    }
 
-    return 0;
+    // check if zero
+    if (num == 0)
+    {
+        buf[0] = '0';
+	buf[1] = 0;
+	return buf;
+    }
+
+    // check if negative
+    int isNegative = 0;
+    if (num < 0)
+    {
+        buf[0] = '-';
+	num *= -1;
+	isNegative = 1;
+    }
+
+    char reverseBuf[10];
+    int index = 0;
+
+    while (num > 0)
+    {
+        reverseBuf[index] = (char) ((num % 10) + 48);
+	num = num / 10;
+	index++;
+    }
+
+    int size = isNegative + size;
+
+
+
+    return buf;
 }
