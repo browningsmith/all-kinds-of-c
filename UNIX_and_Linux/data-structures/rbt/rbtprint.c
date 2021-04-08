87d37e47e4e16481ad68f8ce85937dc9b45cd6ea
Keith Smith <browningsmith@email.arizona.edu>
2021-04-07 21:44:31 -0700

    added PLine struct and newPLine method to rbtprint

--- UNIX_and_Linux/data-structures/rbt/rbtprint.c
+++ UNIX_and_Linux/data-structures/rbt/rbtprint.c
@@ -1,3 +1,26 @@
 #include "rbtprint.h"
 
+#include <stdlib.h>
+
 #include "include/doublell.h"
+
+PLine* newPLine(void)
+{
+    PLine* newLine = malloc(sizeof(PLine));
+    if (newLine == NULL)
+    {
+        return newLine;
+    }
+
+    newLine->text = malloc(sizeof(100));
+    if (newLine->text == NULL)
+    {
+        free(newLine);
+	return NULL;
+    }
+
+    newLine->capacity = 100;
+    newLine->usage = 0;
+
+    return newLine;
+}
--- UNIX_and_Linux/data-structures/rbt/rbtprint.h
+++ UNIX_and_Linux/data-structures/rbt/rbtprint.h
@@ -1,4 +1,14 @@
 #ifndef RBTPRINT_H
 #define RBTPRINT_H
 
+typedef struct {
+
+	long capacity;
+	long usage;
+	char* text;
+	
+} PLine;
+
+PLine* newPLine(void);
+
 #endif /* RBTPRINT_H */
