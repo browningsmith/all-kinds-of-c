3e8c8db13f1a066b84a230ce4911a78e8c6a51e2
Keith Smith <browningsmith@email.arizona.edu>
2021-04-08 06:44:33 -0700

    Created cooltools, a small library with different useful C tools I done made

---
+++ UNIX_and_Linux/cooltools/cooltests.c
@@ -0,0 +1,12 @@
+#include "cooltools.h"
+
+#include <stdint.h>
+#include <stdio.h>
+
+int main()
+{
+    char buffer[51];
+    itoa(buffer, 47);
+
+    return 0;
+}
---
+++ UNIX_and_Linux/cooltools/itoa.c
@@ -0,0 +1,12 @@
+#include "cooltools.h"
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int itoa(char* buf, int32_t num)
+{
+    printf("Turning %i into words since 2021!\n", (int) num);
+
+    return 0;
+}
