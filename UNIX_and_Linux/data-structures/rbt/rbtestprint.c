c72df24cb073b4a07e9cb2037827b485a72729d6
Keith Smith <browningsmith@email.arizona.edu>
2021-04-07 22:40:19 -0700

    Finished testing newPLine

--- UNIX_and_Linux/data-structures/rbt/rbtestprint.c
+++ UNIX_and_Linux/data-structures/rbt/rbtestprint.c
@@ -1,19 +1,45 @@
 #include "rbtprint.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "rbtprintimpl.h"
 
 int main()
 {
-    printf("rbtestprint startup\n");
+    printf("Running tests on rbtprint\n");
 
-    PLine* line = newPLine();
-    if (line == NULL)
+    // Test newPLine
+    printf("Testing newPLine\n");
     {
-        perror("Unable to create line");
-        return -1;
+        PLine* line = newPLine();
+        if (line == NULL)
+        {
+            perror("newPLine: Error creating new PLine");
+            return -1;
+        }
+        if (line->text == NULL)
+        {
+            printf("newPLine: Error, line->text was set to NULL\n");
+            return -1;
+        }
+        if (line->capacity != 100)
+        {
+            printf("newPLine: Error, line->capacity was not set to 100\n");
+            return -1;
+        }
+        if (line->usage != 0)
+        {
+            printf("newPLine: Error, line->usage was not set to 0\n");
+            return -1;
+        }
+
+        free(line->text);
+        free(line);
     }
+    printf("Completed newPLine\n");
+
+    printf("Tests Complete\n");
 
     return 0;
 }
