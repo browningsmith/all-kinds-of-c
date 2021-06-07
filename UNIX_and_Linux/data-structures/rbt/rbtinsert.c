eceaf150a263ef2c9c882553cd81387643b23bd2
Keith Smith <browningsmith@email.arizona.edu>
2021-06-06 20:35:06 -0700

    Adding a program called rbtinsert that I can use to insert numbers, and it will print the tree after each insert so I can see if it is working

---
+++ UNIX_and_Linux/data-structures/rbt/rbtinsert.c
@@ -0,0 +1,15 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "include/cooltools.h"
+#include "rbt.h"
+#include "rbtprint.h"
+
+int main()
+{
+    printf("rbtinsert startup\n");
+
+    return 0;
+}
