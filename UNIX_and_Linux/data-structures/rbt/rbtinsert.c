b27d06ba773f86bef3bbe25a75365fe1fb0b8880
Keith Smith <browningsmith@email.arizona.edu>
2021-06-06 20:47:33 -0700

    Added functions for tree to compare and print ints

--- UNIX_and_Linux/data-structures/rbt/rbtinsert.c
+++ UNIX_and_Linux/data-structures/rbt/rbtinsert.c
@@ -7,9 +7,46 @@
 #include "rbt.h"
 #include "rbtprint.h"
 
+int compareInt(void* a, void* b);
+int testitoa(char* buffer, void* content);
+
 int main()
 {
-    printf("rbtinsert startup\n");
+    RBT tree;
+    rbtInit(&tree, compareInt);
+
+    rbtPrint(tree, testitoa);
 
     return 0;
+}
+
+int compareInt(void* a, void* b)
+{
+    int derefA = *((int*) a);
+    int derefB = *((int*) b);
+
+    //printf("Comparing integers A: %i and B: %i\n", derefA, derefB);
+
+    if (derefA == derefB)
+    {
+        //printf("A is equal to B\n");
+        return 0;
+    }
+    else if (derefA < derefB)
+    {
+        //printf("A is less than B\n");
+        return -1;
+    }
+    else
+    {
+       // printf("A is greater than B\n");
+        return 1;
+    }
+}
+
+int testitoa(char* buffer, void* content)
+{
+    itoa(buffer, *(int32_t*) content);
+    
+    return strlen(buffer);
 }
