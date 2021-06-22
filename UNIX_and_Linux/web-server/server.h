49a9efa0e7a2d5ab46931b889c09d021ffb8d096
Keith Smith <browningsmith@email.arizona.edu>
2021-06-21 19:35:55 -0700

    Removed the original code, want to start fresh with a single source file

--- UNIX_and_Linux/web-server/server.h
+++
@@ -1,7 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-
-#define PORT 8080
-#define BACKLOG 10
-
-#endif /* SERVER_H */
---
+++ UNIX_and_Linux/web-server/webserver.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("webserver startup\n");
+
+    return 0;
+}
